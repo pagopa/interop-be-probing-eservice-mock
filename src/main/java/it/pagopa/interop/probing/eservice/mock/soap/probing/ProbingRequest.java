40f5b0638f352795d954937be938880fd318d05c
gdellorusso <giuseppe.dellorusso@dxc.com>
2023-07-31 17:39:10 +0200

    Minor fix

--- src/main/java/it/pagopa/interop/probing/eservice/mock/soap/probing/ProbingRequest.java
+++ src/main/java/it/pagopa/interop/probing/eservice/mock/soap/probing/ProbingRequest.java
@@ -2,11 +2,11 @@
 // Questo file è stato generato dall'Eclipse Implementation of JAXB, v3.0.0 
 // Vedere https://eclipse-ee4j.github.io/jaxb-ri 
 // Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2023.05.25 alle 05:57:55 PM CEST 
+// Generato il: 2023.07.31 alle 05:35:14 PM CEST 
 //
 
 
-package it.pagopa.interop.probing.eservice.mock.soap.probing.v1;
+package it.pagopa.interop.probing.eservice.mock.soap.probing;
 
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
